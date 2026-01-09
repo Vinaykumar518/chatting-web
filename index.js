80d48410d86337a9775719595a75bdc0c698034d
Vinaykumar518 <bhukyavinay445@gmail.com>
2026-01-09 17:25:00 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import path from "path";
+
+import { connectDB } from "./lib/db.js";
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import { app, server } from "./lib/socket.js";
+
+dotenv.config(); // ✅ load .env before using process.env
+
+const PORT = process.env.PORT;
+const __dirname = path.resolve();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: ["https://chatting-app-rlv4.onrender.com"], // your frontend domain on Render
+    credentials: true,
+  })
+);
+
+// Routes
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
+// Serve frontend in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
+// Start server
+server.listen(PORT, () => {
+  console.log("✅ Server running on PORT:", PORT);
+  connectDB();
+});
